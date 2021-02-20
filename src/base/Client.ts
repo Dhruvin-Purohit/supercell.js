2974c9ff4bf47549d91ee7508f01643db6ba703c
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-02-20 20:20:57 +0530

    Only one more error, ts is unable to find module: events, idfk why

--- src/base/Client.ts
+++ src/base/Client.ts
@@ -1,14 +1,12 @@
-import EventEmitter from "node:events";
-import * as BSClient from '../brawlStars/Client'
+import { EventEmitter } from "events";
+import BSClient from '../brawlStars/Client'
 
 /**
  * The base client for interacting with the supercell api's
  */
 export default class Client extends EventEmitter {
-    public constrcutor(BSClientOptions: ClientOptions/*, COCClientOptions: ClientOptions, CRClientOptions: ClientOptions*/) {
-        //@ts-ignore tf is wrong here?
+    public constructor(BSClientOptions: ClientOptions/*, COCClientOptions: ClientOptions, CRClientOptions: ClientOptions*/) {
         super();
-        //@ts-ignore
         this.BSClient = new BSClient(BSClientOptions)
     }
     public BSClient: any
